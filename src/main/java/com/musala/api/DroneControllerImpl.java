cfe5c103ca9908a59b25a3ca6f98222871f19c92
Tanaka Machana <tmachana@afcholdings.co.zw>
2023-02-07 21:38:38 +0200

    add: DroneControllerImpl local controller advice

--- src/main/java/com/musala/api/DroneControllerImpl.java
+++ src/main/java/com/musala/api/DroneControllerImpl.java
@@ -7,11 +7,18 @@ import com.musala.model.Drone;
 import com.musala.service.DroneService;
 import com.musala.service.base.JpaCrudService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -42,5 +49,19 @@ public class DroneControllerImpl extends AbstractApi<DroneDto, Drone> implements
 	protected Drone map(DroneDto var1) {
 
 		return mapper.map(var1);
+	}
+
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public Map<String, String> handleValidationExceptions(
+			MethodArgumentNotValidException ex) {
+
+		Map<String, String> errors = new HashMap<>();
+		ex.getBindingResult().getAllErrors().forEach((error) -> {
+			String fieldName = ((FieldError) error).getField();
+			String errorMessage = error.getDefaultMessage();
+			errors.put(fieldName, errorMessage);
+		});
+		return errors;
 	}
 }
