34e433ffa5bca48ce4edf3d50c87026c429027c7
Tanaka Machana <tmachana@afcholdings.co.zw>
2023-02-08 09:32:17 +0200

    add: handleDataIntegrityException ControllerAdvice modify: handleNodataFoundException to handleValidationException

--- src/main/java/com/musala/api/ControllerAdvice.java
+++ src/main/java/com/musala/api/ControllerAdvice.java
@@ -1,6 +1,7 @@
 package com.musala.api;
 
 import com.musala.exception.ValidationException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -16,7 +17,7 @@ import java.util.Map;
 public class ControllerAdvice extends ResponseEntityExceptionHandler {
 
 	@ExceptionHandler(ValidationException.class)
-	public ResponseEntity<Object> handleNodataFoundException(
+	public ResponseEntity<Object> handleValidationException(
 			ValidationException ex) {
 
 		Map<String, Object> body = new LinkedHashMap<>();
@@ -24,6 +25,17 @@ public class ControllerAdvice extends ResponseEntityExceptionHandler {
 		body.put("message", ex.getMessage());
 
 		return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(DataIntegrityViolationException.class)
+	public ResponseEntity<Object> handleDataIntegrityException(
+			DataIntegrityViolationException ex) {
+
+		Map<String, Object> body = new LinkedHashMap<>();
+		body.put("timestamp", LocalDateTime.now());
+		body.put("message", "Duplicate record");
+
+		return new ResponseEntity<>(body, HttpStatus.CONFLICT);
 	}
 
 }
